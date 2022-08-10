50e2ded3525c44a6c945fd51dc2c480a39cb60b0
Alkarias <alkarias29@gmail.com>
2022-08-10 09:39:03 -0400

    updated post request to include id

--- api/notes.js
+++ api/notes.js
@@ -1,33 +1,53 @@
 const notes = require('express').Router();
 const fs = require('fs');
 
+
+//tell the application where the information is saved
+const fileName = 'db/db.json';
+
+
 //  /api/notes
+// get all of the saved notes
 notes.get('/', (req, res) => {
-   fs.readFile('db/db.json', 'utf-8', (err, data) => {
-    err ? console.error(err) : res.json(JSON.parse(data));
-   });
+    try {
+        fs.readFile(fileName, 'utf-8', (err, data) => {
+            err ? console.error(err) : res.json(JSON.parse(data));
+        });
+    } catch (err) {
+        res.json(err);
+    }
 });
 
 notes.post('/', (req, res) => {
+    // instantiate the information entered onto the page
     const { title, text } = req.body;
-    
-    const newNote = {
-        title,
-        text
-    }
-
-    const noteData = fs.readFile('db/db.json', 'utf-8', (err, data) => {
+ 
+    //get the list of stored information
+    fs.readFile(fileName, 'utf-8', (err, data) => {
+        // if there is an error, console log it
+        // otherwise, run the code to append the array of list items
         if (err) console.error(err); 
-        return JSON.parse(data);
+        else {
+            // parses the information from the .json file and makes it usable
+            const noteData = JSON.parse(data);
+            // make a new note object that stores the information
+            const newNote = {
+                id: data.length,
+                title,
+                text
+            }
+            // adds the new note to the array of notes
+            noteData.push(newNote);
+            //writes the information to the json file
+            fs.writeFile(fileName, JSON.stringify(noteData), err => console.error(err));
+            res.json(noteData);
+        }
     });
-
-    noteData.push(newNote);
-    
-    res.json();
 });
 
 notes.delete('/:id', (req, res) => {
-    res.json();
+    console.log(req.params.id);
+    res.json('all good');
 });
 
 module.exports = notes;
